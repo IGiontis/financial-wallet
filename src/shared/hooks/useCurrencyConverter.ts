bcb3bb498cd88580cc2066bcc7735b9aa16d071b
IGiontis <iliasgiontis@gmail.com>
2026-04-10 03:50:33 +0300

    currency fix

--- src/shared/hooks/useCurrencyConverter.ts
+++ src/shared/hooks/useCurrencyConverter.ts
@@ -55,8 +55,8 @@ export function useCurrencyConverter() {
     return new Intl.NumberFormat("en-US", {
       style: "currency",
       currency: displayCurrency,
-      minimumFractionDigits: 0,
-      maximumFractionDigits: 0,
+      minimumFractionDigits: 2,
+      maximumFractionDigits: 2,
     }).format(converted);
   };
 
